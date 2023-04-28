28ee7c9cb774204dfedfc35a6958a92b392ede95
KJBhadouria <kumarijya101251@gmail.com>
2023-04-28 19:29:58 +0530

    base files

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Login from "./src/screens/login/login";
 import Routes from "./src/screens/routes";
 
 export default function App() {
